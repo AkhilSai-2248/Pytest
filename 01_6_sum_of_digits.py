c386099724354acbb89e56cbc75f93b69566340c
AkhilSai <9963109540@gitam.in>
2021-06-05 16:02:48 +0530

    first comit

--- 01_6_sum_of_digits.py
+++ 01_6_sum_of_digits.py
@@ -1,3 +1,9 @@
 """ Python Script to find sum of digits of a number """
 
 # Write your code from here
+
+num = input("Enter a number: ")
+sum = 0
+for i in num :
+    sum = sum + int(i)
+print("Sum of Digits of a number:",sum)
